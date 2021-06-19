e793a281c928191a780523e8877ce071ed92b094
Isaac Souza <isaacsouzawrk@gmail.com>
2021-06-19 16:44:57 -0300

    Alterando lista de perguntas para Map

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,23 @@ class _PerguntaAppState extends State<PerguntaApp>{
   }
   @override
   Widget build(BuildContext context){
-    final  perguntas = [
-      'Qual deus Nórdico é conhecido como pai de todos?',
-      'Qual deus grego é o mensageiro dos deuses ?',
-      'Qual religião pertence o deus Barão Samedi?',
-      'de qual mitologia pertence o deus Amon?',
-      'qual mitologia associou deuses gregos a nome de planetas?',
-      'Sleipnir, o cavalo de Odin, tem como mãe:'
+    final  List<Map> perguntas = [
+      {
+        'texto':'Qual a qual Mitologia pertence o deus Barão Samedi?',
+        'respostas':['Voodo','Maia','Nórdica','Celta'],
+      },
+      { 'texto':'Qual é o deus da chuva maia?',
+        'respostas':['Chaac','Ah puch','Cabrakan','Camazotz'],
+      },
+      { 'texto':'Qual deus nórdico teve sua mão decepada?',
+        'respostas':['Tyr','Fenrir','Freya','Odin'],
+      },
+      { 'texto':'Qual mitologia associa os nomes dos deuses a planetas?',
+        'respostas':['Voodo','Grega','Egipcia','Romana'],
+        },
+      { 'texto':'Na mitologia grega, qual é considerado o mensageiro dos deuses?',
+        'respostas':['Zeus','Athena','Apollo','Mercúrio'],
+        }
     ];
     return MaterialApp(
       theme:ThemeData(
@@ -34,7 +44,7 @@ class _PerguntaAppState extends State<PerguntaApp>{
         ),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]['texto']),
             Resposta('Resposta 1',_responder),
             Resposta('Resposta 2',_responder),
             Resposta('Resposta 3',_responder),
