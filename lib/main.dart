65c89a483456a243fc73e850cff475fda61fd3d7
Isaac Souza <isaacsouzawrk@gmail.com>
2021-06-19 18:01:12 -0300

    Refatorando Widget de Resultado

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,14 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
+import './resultado.dart';
 
 main()=>runApp(new PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp>{
   var _perguntaSelecionada = 0;
 
-  void _responder(){
-    setState((){
-      _perguntaSelecionada++;
-    });
-  }
-  @override
-  Widget build(BuildContext context){
-    final  List<Map> perguntas = [
+  final  List<Map> _perguntas = const [
       {
         'texto':'Qual a qual Mitologia pertence o deus Barão Samedi?',
         'respostas':['Voodo','Maia','Nórdica','Celta'],
@@ -34,8 +28,20 @@ class _PerguntaAppState extends State<PerguntaApp>{
         }
     ];
 
-    List<String> respostas = perguntas[_perguntaSelecionada].cast()['respostas'];
+  void _responder(){
+    setState((){
+      _perguntaSelecionada++;
+    });
+  }
+
+  bool get temPerguntaSelecionada{
+    return _perguntaSelecionada < _perguntas.length;
+  }
+  @override
+  Widget build(BuildContext context){
     
+
+    List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada].cast()['respostas'] : [];
     //for (String textoResp in respostas) {
       //widgets.add(Resposta(textoResp, _responder));
     //}
@@ -48,12 +54,12 @@ class _PerguntaAppState extends State<PerguntaApp>{
           title: Text('Quiz da Mitologia'),
           centerTitle: true,
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
+            Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
             ... respostas.map((t) =>Resposta(t,_responder)).toList(),
           ],
-        ),
+        ):Resultado()
       ),
     );
   }
---
+++ lib/resultado.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: Text('Parabéns',
+          style: TextStyle(fontSize: 18),),
+    );
+  }
+}
